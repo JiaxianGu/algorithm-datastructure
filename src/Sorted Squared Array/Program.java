0d0bd42ea27071f7bda8ba62ba345548841c7fc6
JiaxianGu <gujiaxian1997@gmail.com>
2024-04-02 00:31:52 +0900

    sorted squared array java solution

---
+++ src/Sorted Squared Array/Program.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Program {
+
+  public int[] sortedSquaredArray(int[] array) {
+    int[] result = new int[array.length];
+    int left = 0;
+    int right = array.length - 1;
+    for (int i = array.length - 1; i >= 0; i--) {
+      if (Math.abs(array[right]) > Math.abs(array[left])) {
+        result[i] = array[right] * array[right];
+        right -= 1;
+      } else {
+        result[i] = array[left] * array[left];
+        left += 1;
+      }
+    }
+    return result;
+  }
+}
